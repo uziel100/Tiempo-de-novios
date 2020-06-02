47a0c23ea2da4b137a06eb68c02876fa87f913cc
Uziel <uzielmelitonh@gmail.com>
2020-06-02 12:36:33 -0500

    Segundo commit

--- src/app/Time.js
+++ src/app/Time.js
@@ -1,22 +1,32 @@
 export default class Time{
     constructor(date, moment){
+        this.day = date.day;
         this.moment = moment;        
-        this.startDate = this.moment([date.year, date.month, date.day]);        
-        this.currentDate = this.moment([this.moment().year(), this.moment().month() + 1, (this.moment().date() == 31)? 30 : this.moment().date()]);        
-        this.dateAnniversary = this.moment([this.moment().year(), this.moment().month() + 1, date.day ]);                        
+        this.startDate = this.moment([date.year, date.month - 1, this.day]);        
+        this.currentDate = this.moment([this.moment().year(), this.moment().month(),  this.moment().date() ]);        
     }
  
      getTimeTogether(){
         const totalMonths =  this.currentDate.diff(this.startDate, 'months');
         const years = parseInt(totalMonths / 12);
         const months = totalMonths % 12;
-        const days = (this.moment().date() == 31)? this.currentDate.diff(this.dateAnniversary, 'days') + 1: this.currentDate.diff(this.dateAnniversary, 'days');                
-        
+        const days = this.getDays();
+     
         return{
             years: years,
             months: months,
             days: days
         }
+    }
+
+    getDays(){
+        let totalDias = 0;        
+        const aniversarioPorMes = this.moment([this.moment().year(), this.moment().month(), this.day]);
+        const aniversarioMesAnterior = this.moment([this.moment().year(), this.moment().month() - 1, this.day]);
+
+        totalDias = (this.currentDate < aniversarioPorMes)? this.currentDate.diff(aniversarioMesAnterior, 'days') : this.currentDate.diff(aniversarioPorMes, 'days');        
+             
+        return totalDias;
     }
 
 }
